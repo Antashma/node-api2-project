6c389ac47796898c31ae4a26fefb4ae863f3bd0f
Samantha Goodman <antashma@gmail.com>
2020-10-31 01:15:15 -0400

    add update but get 500 error when checking req body

--- index.js
+++ index.js
@@ -123,6 +123,28 @@ server.delete('/api/posts/:id', (req, res) => {
 /* PUT	/api/posts/:id	
 Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original. */
 server.put('/api/posts/:id', (req, res) => {
+    model.update(req.params.id, req.body)
+        .then(post => {
+            if (req.params.id) {
+                if(req.body.title || req.body.contents) {
+                    res.status(200).json(post)
+                } else {
+                    res.status(400).json({
+                        errorMessage: "Please provide title and contents for the post."
+                    })
+                } 
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: "The post information could not be modified."
+            });
+        })
 
 })
 
